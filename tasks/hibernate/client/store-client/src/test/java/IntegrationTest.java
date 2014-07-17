86210c665f24dff10836c9f1642eb07c5f4ae20f
njarek <jojcoto@go2.pl>
2014-07-17 07:44:21 +0200

    update

---
+++ tasks/hibernate/client/store-client/src/test/java/IntegrationTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pl.client.FindBasket;
+import pl.client.NewBasket;
+import pl.client.UpdateBasket;
+import pl.store.domain.Basket;
+
+
+public class IntegrationTest {
+
+	@Test
+	public void test() throws Exception{
+		Basket findBasket = new FindBasket().findBasket(1);
+		assertNotNull(findBasket);		
+		Basket basket= new NewBasket().sendNewBasket();
+		assertNotNull(basket);
+		
+		Basket basket2=new UpdateBasket().updateBasket(basket.getId());
+		assertNotNull(basket2);
+		Basket basket3=new UpdateBasket().updateBasket(basket.getId());
+		assertNotNull(basket3);
+		
+		Basket basket4= new NewBasket().sendNewBasket();
+		assertNotNull(basket4);
+		Basket basket5= new NewBasket().sendNewBasket();
+		assertNotNull(basket5);
+	}
+}
