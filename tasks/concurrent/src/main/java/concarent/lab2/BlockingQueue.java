af732eb53c28739547fa55f4c7fe128dcf298792
njarek <jojcoto@go2.pl>
2014-08-25 09:11:55 +0200

    producent consument

---
+++ tasks/concurrent/src/main/java/concarent/lab2/BlockingQueue.java
@@ -0,0 +1,49 @@
+package concarent.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+
+import concarent.lab1.JareksLock;
+
+public class BlockingQueue {
+	private List<String> lista = new ArrayList<String>();
+	int bufforSize;
+	int currentSize=0;
+	
+	public BlockingQueue(int size) {
+		this.bufforSize=size;
+	}
+	
+	public  void put(String message, String threadname) throws InterruptedException{
+		System.out.println(threadname+" is waiting to sent message");
+		synchronized (lista) {
+			
+		
+		while(bufforSize<=currentSize){
+			lista.wait();
+		}
+
+		currentSize++;
+		lista.add(message);
+		lista.notify();
+		}
+		
+	}
+	
+	public synchronized String take() throws InterruptedException{
+		System.out.println("consumet is waiting to take messsage");
+		synchronized (lista) {
+		while(currentSize<=0){
+			lista.wait();
+		}
+		currentSize--;
+		String string=lista.get(0);
+		lista.remove(0);
+		lista.notify();
+		return string;
+		}
+		
+	}
+	
+}
---
+++ tasks/concurrent/src/main/java/concarent/lab2/ProducentConsument.java
@@ -0,0 +1,66 @@
+package concarent.lab2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProducentConsument {
+
+	public static void main(String[] args) {
+		ProducentConsument producentConsument = new ProducentConsument();
+		producentConsument.go();
+	}
+
+	BlockingQueue blockingQueue = new BlockingQueue(5);
+
+	private void go() {
+		Thread producent = new Thread(new Runnable() {
+
+			public void run() {
+				try {
+					sent("producent");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		});
+
+		Thread consument = new Thread(new Runnable() {
+
+			public void run() {
+				try {
+					take("consument");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		});
+		producent.start();
+		consument.start();
+	}
+
+	private void sent(String threadname) throws InterruptedException {
+		int y = 0;
+		for (int i = 0; i < 100; i++) {
+			Thread.sleep((long) (Math.random() * 500));
+			String message = "" + "" + Math.random();
+			blockingQueue.put(message, threadname);
+			System.out.println("producent sent " + message + " message " + y++);
+		}
+	}
+
+	private void take(String string) throws InterruptedException {
+		Thread.sleep(2000);
+		for (int i = 0; i < 100; i++) {
+			Thread.sleep(1000);
+
+			System.out.println("consumet take " + blockingQueue.take() + " "
+					+ i);
+		}
+
+	}
+
+}
